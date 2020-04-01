68bb8e3b269a39cd0adea55371c4ebfb884b962a
Ludovic Dureau <ludodureau@gmail.com>
2020-04-01 16:44:06 +0200

    Events moved in the database and code refactored

---
+++ connec.php
@@ -0,0 +1,4 @@
+<?php
+define('DSN', 'mysql:host=localhost;dbname=projet1;charset=utf8');
+define('USER', 'root');
+define('PASS', 'password');
---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <a href="#" class="logo"><img class="small-img" src="../images/logo-galet.png" alt="logo"></a>
+    <a href="#menu" class="menu-hidden"><div class="item-menu">Notre menu</div></a>
+    <a href="#evenements" class="menu-hidden"><div class="item-menu">Nos événements</div></a>
+    <a href="#contact" class="menu-hidden"><div class="item-menu">Contactez-nous</div></a>
+    <a href="#reseaux" class="menu-hidden"><div class="item-menu">Où sommes-nous ?</div></a>
+    <div class="burger-title">Le Galet du Jardin Botanique</div>
+    <div class="burger" id="burger" ><i class="fas fa-bars burger-icon"></i></div>
+</nav>
